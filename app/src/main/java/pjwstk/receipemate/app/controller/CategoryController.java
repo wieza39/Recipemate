e2344710b347f06cde259de6ca70acdcb477682f
wolenkowicz <olenkowiczwojciech@gmail.com>
2023-12-05 21:21:57 +0100

    Create GetAllCategories Endpoint

--- app/src/main/java/pjwstk/receipemate/app/controller/CategoryController.java
+++ app/src/main/java/pjwstk/receipemate/app/controller/CategoryController.java
@@ -9,16 +9,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import pjwstk.receipemate.app.view.CategoryView;
 import pjwstk.receipemate.app.view.PageView;
 import pjwstk.receipemate.app.viewrepository.CategoryRecipesPageViewRepository;
+import pjwstk.receipemate.app.viewrepository.CategoryViewRepository;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/category")
 public class CategoryController {
     private final CategoryRecipesPageViewRepository categoryRecipesPageViewRepository;
 
-    public CategoryController(CategoryRecipesPageViewRepository categoryRecipesPageViewRepository) {
+    private final CategoryViewRepository categoryViewRepository;
+
+    public CategoryController(
+            CategoryRecipesPageViewRepository categoryRecipesPageViewRepository,
+            CategoryViewRepository categoryViewRepository
+    ) {
         this.categoryRecipesPageViewRepository = categoryRecipesPageViewRepository;
+        this.categoryViewRepository = categoryViewRepository;
     }
 
     @GetMapping("/{id}/recipes")
@@ -30,5 +40,12 @@ public class CategoryController {
     ) {
         Pageable pageable = PageRequest.of(pageNumber, limit);
         return ResponseEntity.ok(categoryRecipesPageViewRepository.getList(pageable, categoryId));
+    }
+
+    @GetMapping
+    @ResponseBody
+    public ResponseEntity<List<CategoryView>> getAll() {
+
+        return ResponseEntity.ok(categoryViewRepository.getList());
     }
 }
---
+++ app/src/main/java/pjwstk/receipemate/app/viewrepository/CategoryViewRepository.java
@@ -0,0 +1,26 @@
+package pjwstk.receipemate.app.viewrepository;
+
+import org.springframework.stereotype.Service;
+import pjwstk.receipemate.app.entity.Category;
+import pjwstk.receipemate.app.repository.CategoryRepository;
+import pjwstk.receipemate.app.view.CategoryView;
+import pjwstk.receipemate.app.viewfactory.CategoryViewFactory;
+
+import java.util.List;
+
+@Service
+public class CategoryViewRepository {
+    private final CategoryRepository categoryRepository;
+    private final CategoryViewFactory categoryViewFactory;
+
+    public CategoryViewRepository(CategoryRepository categoryRepository, CategoryViewFactory categoryViewFactory) {
+        this.categoryRepository = categoryRepository;
+        this.categoryViewFactory = categoryViewFactory;
+    }
+
+    public List<CategoryView> getList() {
+        List<Category> categories = this.categoryRepository.findAll();
+
+        return this.categoryViewFactory.makeList(categories);
+    }
+}
