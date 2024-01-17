d7b79baf81974e51b9c887fd6077a53818cc4a0a
wolenkowicz <olenkowiczwojciech@gmail.com>
2024-01-17 22:38:11 +0100

    Data consistency with api

--- app/src/main/resources/data.sql
+++ app/src/main/resources/data.sql
@@ -1,72 +1,61 @@
 INSERT INTO recipemate.category (id, name)
-VALUES (1, 'Italian Cuisine');
+VALUES (1, 'Kuchnia włoska');
 INSERT INTO recipemate.category (id, name)
-VALUES (2, 'Asian Flavors');
+VALUES (2, 'Azjatyckie smaki');
 INSERT INTO recipemate.category (id, name)
-VALUES (3, 'Desserts');
+VALUES (3, 'Desery');
 
 INSERT INTO recipemate.recipe (category_id, created_at, id, updated_at, description, difficulty, name, time_consuming,
                                portion_count)
-VALUES (1, '2023-01-01 12:00:00.000000', 1, '2023-01-01 14:30:00.000000', '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac dolor eu elit interdum euismod.</p>
-<p>Vestibulum vel turpis vel libero scelerisque ullamcorper eget sed lorem. Curabitur aliquet lectus vel massa iaculis, eu ultrices arcu convallis.</p>
-<p>Praesent sit amet tortor eu elit convallis cursus. Nunc eu lectus sed justo pellentesque tincidunt. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>',
-        'MEDIUM',
+VALUES (1, '2023-01-01 12:00:00.000000', 1, '2023-01-01 14:30:00.000000', '<p>Tradycyjne włoskie danie, które zawsze smakuje wyjątkowo. Pyszne spaghetti podane z sosem Bolognese, przygotowanym z mięsa mielonego, pomidorów, cebuli i przypraw.</p>
+<p>To danie o średnim stopniu trudności sprawi, że poczujesz się jak w prawdziwej włoskiej trattorii.</p>',
+        'Średni',
         'Spaghetti Bolognese', '45', 3),
-       (2, '2023-01-02 10:30:00.000000', 2, '2023-01-02 12:45:00.000000', '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac dolor eu elit interdum euismod.</p>
-<p>Vestibulum vel turpis vel libero scelerisque ullamcorper eget sed lorem. Curabitur aliquet lectus vel massa iaculis, eu ultrices arcu convallis.</p>
-<p>Praesent sit amet tortor eu elit convallis cursus. Nunc eu lectus sed justo pellentesque tincidunt. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>',
-        'HARD', 'Dragon Roll Sushi',
+       (2, '2023-01-02 10:30:00.000000', 2, '2023-01-02 12:45:00.000000', '<p>Dragon Roll Sushi to wyjątkowe danie kuchni azjatyckiej. Sushi zawinięte w kształt smoka z delikatnym nadzieniem rybnym, awokado i ogórkiem. Podane z sosem sojowo-imbirowym, to danie z pewnością oczaruje smakoszy kuchni orientalnej.</p>
+<p>Przygotowanie tego dania jest trudniejsze, ale efekt z pewnością wynagrodzi wysiłek.</p>',
+        'Trudny', 'Dragon Roll Sushi',
         '60', 2),
-       (3, '2023-01-03 15:20:00.000000', 3, '2023-01-03 17:10:00.000000', '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac dolor eu elit interdum euismod.</p>
-<p>Vestibulum vel turpis vel libero scelerisque ullamcorper eget sed lorem. Curabitur aliquet lectus vel massa iaculis, eu ultrices arcu convallis.</p>
-<p>Praesent sit amet tortor eu elit convallis cursus. Nunc eu lectus sed justo pellentesque tincidunt. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>',
-        'EASY', 'Buttery Croissants',
+       (3, '2023-01-03 15:20:00.000000', 3, '2023-01-03 17:10:00.000000', '<p>Rogaliki maślane to pyszne i puszyste ciasteczka, które rozpływają się w ustach. Idealne na śniadanie lub podwieczorek. Przygotowanie ich jest łatwe i przyjemne, a efekt z pewnością zachwyci Twoje podniebienie.</p>
+<p>Zapraszamy do spróbowania tego łatwego przepisu na słodki smakołyk!</p>',
+        'Łatwy', 'Rogaliki maślane',
         '120', 5),
-       (1, '2023-01-04 09:45:00.000000', 4, '2023-01-04 11:30:00.000000', '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac dolor eu elit interdum euismod.</p>
-<p>Vestibulum vel turpis vel libero scelerisque ullamcorper eget sed lorem. Curabitur aliquet lectus vel massa iaculis, eu ultrices arcu convallis.</p>
-<p>Praesent sit amet tortor eu elit convallis cursus. Nunc eu lectus sed justo pellentesque tincidunt. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>',
-        'MEDIUM',
-        'Authentic Margherita Pizza', '30', 4),
-       (2, '2023-01-05 14:15:00.000000', 5, '2023-01-05 16:00:00.000000', '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac dolor eu elit interdum euismod.</p>
-<p>Vestibulum vel turpis vel libero scelerisque ullamcorper eget sed lorem. Curabitur aliquet lectus vel massa iaculis, eu ultrices arcu convallis.</p>
-<p>Praesent sit amet tortor eu elit convallis cursus. Nunc eu lectus sed justo pellentesque tincidunt. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>',
-        'EASY',
-        'Teriyaki Chicken and Rice Bowl', '40', 2),
-       (3, '2023-01-06 11:00:00.000000', 6, '2023-01-06 13:20:00.000000', '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac dolor eu elit interdum euismod.</p>
-<p>Vestibulum vel turpis vel libero scelerisque ullamcorper eget sed lorem. Curabitur aliquet lectus vel massa iaculis, eu ultrices arcu convallis.</p>
-<p>Praesent sit amet tortor eu elit convallis cursus. Nunc eu lectus sed justo pellentesque tincidunt. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>',
-        'HARD',
-        'Decadent Chocolate Mousse', '90', 6),
-       (1, '2023-01-07 16:30:00.000000', 7, '2023-01-07 18:45:00.000000', '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac dolor eu elit interdum euismod.</p>
-<p>Vestibulum vel turpis vel libero scelerisque ullamcorper eget sed lorem. Curabitur aliquet lectus vel massa iaculis, eu ultrices arcu convallis.</p>
-<p>Praesent sit amet tortor eu elit convallis cursus. Nunc eu lectus sed justo pellentesque tincidunt. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>',
-        'MEDIUM', 'Hearty Beef Lasagna',
+       (1, '2023-01-04 09:45:00.000000', 4, '2023-01-04 11:30:00.000000', '<p>Pizza Margherita to klasyczne danie włoskie, które zachwyca prostotą i smakiem. Delikatne ciasto, pomidory, mozzarella i świeża bazylia tworzą kompozycję idealną dla miłośników kuchni śródziemnomorskiej.</p>
+<p>Przygotowanie tego dania o średnim stopniu trudności nie zajmie Ci wiele czasu, a efekt z pewnością będzie smakowity.</p>',
+        'Średni',
+        'Pizza Margherita', '30', 4),
+       (2, '2023-01-05 14:15:00.000000', 5, '2023-01-05 16:00:00.000000', '<p>Kurczak Teryiaki z ryżem to danie inspirowane kuchnią japońską. Soczyste kawałki kurczaka w aromatycznym sosie Teryiaki, podane z ryżem - to połączenie smaków, które z pewnością przypadnie do gustu miłośnikom kuchni azjatyckiej.</p>
+<p>Przygotowanie tego łatwego dania zajmie zaledwie 40 minut!</p>',
+        'Łatwy',
+        'Kurczak Teryiaki z ryżem', '40', 2),
+       (3, '2023-01-06 11:00:00.000000', 6, '2023-01-06 13:20:00.000000', '<p>Mus czekoladowy to pyszny deser, który zadowoli wszystkich łasuchów. Delikatny mus z gorzkiej czekolady podany w eleganckich kieliszkach to doskonały sposób na zakończenie obiadu lub romantyczną kolację.</p>
+<p>Choć przygotowanie musu jest trudniejsze, efekt z pewnością wynagrodzi wszelkie starania.</p>',
+        'Łatwy',
+        'Mus czekoladowy', '90', 6),
+       (1, '2023-01-07 16:30:00.000000', 7, '2023-01-07 18:45:00.000000', '<p>Lasagna z wołowiną to klasyczne danie kuchni włoskiej, które uwodzi aromatem i smakiem. Warstwy makaronu, mięsa wołowego, sosu pomidorowego i sera tworzą niezapomnianą kompozycję.</p>
+<p>Przygotowanie tego dania o średnim stopniu trudności z pewnością będzie przyjemnością dla miłośników kuchni włoskiej.</p>',
+        'Średni', 'Lasagna z wołowiną',
         '75', 4),
-       (2, '2023-01-08 12:45:00.000000', 8, '2023-01-08 14:15:00.000000', '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac dolor eu elit interdum euismod.</p>
-<p>Vestibulum vel turpis vel libero scelerisque ullamcorper eget sed lorem. Curabitur aliquet lectus vel massa iaculis, eu ultrices arcu convallis.</p>
-<p>Praesent sit amet tortor eu elit convallis cursus. Nunc eu lectus sed justo pellentesque tincidunt. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>',
-        'EASY',
-        'Soy-Ginger Glazed Salmon', '35', 2),
-       (3, '2023-01-09 10:00:00.000000', 9, '2023-01-09 11:45:00.000000', '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac dolor eu elit interdum euismod.</p>
-<p>Vestibulum vel turpis vel libero scelerisque ullamcorper eget sed lorem. Curabitur aliquet lectus vel massa iaculis, eu ultrices arcu convallis.</p>
-<p>Praesent sit amet tortor eu elit convallis cursus. Nunc eu lectus sed justo pellentesque tincidunt. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>',
-        'MEDIUM',
-        'Cheesecake with Raspberries', '60', 3),
-       (1, '2023-01-10 13:20:00.000000', 10, '2023-01-10 15:00:00.000000', '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac dolor eu elit interdum euismod.</p>
-<p>Vestibulum vel turpis vel libero scelerisque ullamcorper eget sed lorem. Curabitur aliquet lectus vel massa iaculis, eu ultrices arcu convallis.</p>
-<p>Praesent sit amet tortor eu elit convallis cursus. Nunc eu lectus sed justo pellentesque tincidunt. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>',
-        'EASY',
-        'Deluxe Vegetarian Pizza', '40', 4),
-       (2, '2023-01-11 08:30:00.000000', 11, '2023-01-11 10:15:00.000000', '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac dolor eu elit interdum euismod.</p>
-<p>Vestibulum vel turpis vel libero scelerisque ullamcorper eget sed lorem. Curabitur aliquet lectus vel massa iaculis, eu ultrices arcu convallis.</p>
-<p>Praesent sit amet tortor eu elit convallis cursus. Nunc eu lectus sed justo pellentesque tincidunt. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>',
-        'HARD',
-        'Assorted Tempura Platter', '75', 5),
-       (3, '2023-01-12 14:45:00.000000', 12, '2023-01-12 16:30:00.000000', '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac dolor eu elit interdum euismod.</p>
-<p>Vestibulum vel turpis vel libero scelerisque ullamcorper eget sed lorem. Curabitur aliquet lectus vel massa iaculis, eu ultrices arcu convallis.</p>
-<p>Praesent sit amet tortor eu elit convallis cursus. Nunc eu lectus sed justo pellentesque tincidunt. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>',
-        'MEDIUM', 'Apple pie', '50',
+       (2, '2023-01-08 12:45:00.000000', 8, '2023-01-08 14:15:00.000000', '<p>Łosoś w sojowo-imbirowym sosie to danie kuchni azjatyckiej, które wyróżnia się intensywnym smakiem i aromatem. Soczysty łosoś podany z delikatnym sosem sojowo-imbirowym to prawdziwa uczta dla podniebienia.</p>
+<p>Przygotowanie tego łatwego dania zajmie zaledwie 35 minut!</p>',
+        'Łatwy',
+        'Łosoś w sojowo-imbirowym sosie', '35', 2),
+       (3, '2023-01-09 10:00:00.000000', 9, '2023-01-09 11:45:00.000000', '<p>Sernik z malinami to pyszny deser, który zachwyci wszystkich miłośników serników. Delikatne ciasto serowe połączone z kwaśnymi malinami to doskonała propozycja na słodki poczęstunek.</p>
+<p>Przygotowanie tego dania o średnim stopniu trudności zajmie około 60 minut.</p>',
+        'Średni',
+        'Sernik z malinami', '60', 3),
+       (1, '2023-01-10 13:20:00.000000', 10, '2023-01-10 15:00:00.000000', '<p>Pizza wegetariańska to smaczna propozycja dla miłośników warzyw. Delikatne ciasto, sos pomidorowy, świeże warzywa i aromatyczne zioła tworzą idealną kombinację smakową.</p>
+<p>Przygotowanie tej wegetariańskiej pizzy o łatwym stopniu trudności zajmie zaledwie 40 minut!</p>',
+        'Łatwy',
+        'Pizza wegetariańska', '40', 4),
+       (2, '2023-01-11 08:30:00.000000', 11, '2023-01-11 10:15:00.000000', '<p>Mieszany zestaw tempury to danie kuchni japońskiej, które zachwyca delikatnością i chrupkością. Krewetki, kalamary, warzywa - wszystko obtoczone w cienkim cieście i smażone na złoto.</p>
+<p>Przygotowanie tego dania o trudnym stopniu trudności zajmie około 75 minut, ale efekt z pewnością będzie wyjątkowy.</p>',
+        'Trudny',
+        'Mieszany zestaw tempury', '75', 5),
+       (3, '2023-01-12 14:45:00.000000', 12, '2023-01-12 16:30:00.000000', '<p>Jabłecznik to tradycyjne ciasto, które zawsze cieszy się popularnością. Delikatne ciasto poddane magii jabłek, cynamonu i cukru tworzy pyszny deser idealny na każdą okazję.</p>
+<p>Przygotowanie tego dania o średnim stopniu trudności zajmie około 50 minut.</p>',
+        'Średni', 'Jabłecznik', '50',
         3);
+
 
 -- INSERT INTO recipemate.user (id, first_name, email, password, second_name)
 -- VALUES (1, 'JohnDoe', 'johndoe123@test.com', 'pass123', 'Doe');
@@ -167,51 +156,196 @@ VALUES (1, 1, 'https://images.unsplash.com/photo-1498579150354-977475b7ea0b?q=80
        (3, 3, 'https://images.unsplash.com/photo-1486427944299-d1955d23e34d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 'Desserts', 'Desserts');
 
 INSERT INTO ingredient (id, name)
-VALUES (1, 'Tomato'),
-       (2, 'Pasta'),
-       (3, 'Tomato'),
-       (4, 'Onion'),
-       (5, 'Garlic'),
-       (6, 'Basil'),
-       (7, 'Olive Oil'),
+VALUES (1, 'Pomidor'),
+       (2, 'Makaron'),
+       (3, 'Ziemniak'),
+       (4, 'Cebula'),
+       (5, 'Czosnek'),
+       (6, 'Bazylia'),
+       (7, 'Oliwa z oliwek'),
        (8, 'Mozzarella'),
-       (9, 'Salt'),
-       (10, 'Pepper');
+       (9, 'Sól'),
+       (10, 'Pieprz'),
+       (11, 'Boczek'),
+       (12, 'Marchewka'),
+       (14, 'Łodyga selera naciowego'),
+       (15, 'Wołowina'),
+       (16, 'Czerwone Wino'),
+       (17, 'Bulion'),
+       (18, 'Koncentrat pomidorowy'),
+       (19, 'Mleko'),
+       (20, 'Parmezan'),
+       (21, 'Obrane pomidory'),
+       (22, 'Ogórek'),
+       (23, 'Awokado'),
+       (24, 'Nori'),
+       (25, 'Ryż'),
+       (26, 'Krewetki w tempurze'),
+       (27, 'Tobiko'),
+       (28, 'Węgorz'),
+       (29, 'Ocet ryżowy'),
+       (30, 'Woda'),
+       (31, 'Sezam'),
+       (32, 'Drożdże'),
+       (33, 'Masło'),
+       (34, 'Mąka pszenna'),
+       (35, 'Cukier'),
+       (36, 'Żółtko'),
+       (37, 'Oregano'),
+       (38, 'Mozaarella (kulki)'),
+       (39, 'Filet z kurczaka'),
+       (40, 'Olej roślinny'),
+       (41, 'Sos sojowy'),
+       (42, 'Syrop klonowy'),
+       (43, 'Starty imbir'),
+       (44, 'Brązowy ryż'),
+       (45, 'Czerwona papryka'),
+       (46, 'Zielony groszek'),
+       (47, 'Olej sezamowy'),
+       (48, 'Śmietanka 30%'),
+       (49, 'Mascarpone'),
+       (50, 'Gorzka czekolada'),
+       (51, 'Jajko'),
+       (52, 'Passata pomidorowa'),
+       (53, 'Tymianek'),
+       (54, 'Filet łososia'),
+       (55, 'Sok z limonki'),
+       (56, 'Miód'),
+       (57, 'Szczypior'),
+       (58, 'Kolędra'),
+       (59, 'Proszek do pieczenia'),
+       (60, 'Maliny'),
+       (61, 'Jabłko');
+
+
+
 
 INSERT INTO measure (id, name)
-VALUES (1, 'cups'),
-       (2, 'grams'),
-       (3, 'units'),
-       (4, 'medium'),
-       (5, 'cloves'),
-       (6, 'tablespoon'),
-       (7, 'teaspoon'),
-       (8, 'bunch');
+VALUES (1, 'szklanki'),
+       (2, 'gramy'),
+       (3, 'sztuki'),
+       (4, 'średni'),
+       (5, 'ząbki'),
+       (6, 'łyżka stołowa'),
+       (7, 'łyżeczka'),
+       (8, 'puszka'),
+       (9, 'filet'),
+       (10, 'ml'),
+       (11, 'szczypta'),
+       (12, 'liść'),
+       (13, 'pęczek')
 
 INSERT INTO recipe_ingredient (count, ingredient_id, recipe_id, measure_id)
-VALUES (3, 1, 1, 1),   -- 1 'cups'
-       (200, 2, 1, 2), -- 2 'grams'
-       (2, 3, 1, 3),   -- 3 'units'
-       (1, 4, 1, 4),   -- 4 'medium'
-       (50, 5, 1, 2),  -- 2 'grams'
-       (2, 6, 1, 5),   -- 5 'cloves'
-       (1, 7, 1, 6),   -- 6 'tablespoon'
-       (100, 8, 1, 2), -- 2 'grams'
-       (1, 9, 1, 7),   -- 7 'teaspoon'
-       (1, 10, 1, 7); -- 7 'teaspoon'
+VALUES (150, 11, 1, 2),
+       (1, 4, 1, 3),
+       (2, 14, 1, 3),
+       (1, 12, 1, 3),
+       (2, 7, 1, 6),
+       (250, 15, 1, 2),
+       (1, 16, 1, 1),
+       (1, 17, 1, 1),
+       (4, 18, 1, 6),
+       (1, 21, 1, 8),
+       (0.5, 19, 1, 1),
+       (150, 2, 1, 2),
+       (15, 20, 1, 2);
 
 INSERT INTO recipe_ingredient (count, ingredient_id, recipe_id, measure_id)
-VALUES (2, 4, 2, 3),   -- 3 'units'
-       (150, 5, 2, 2), -- 2 'grams'
-       (1, 6, 2, 8),   -- 8 'bunch'
-       (3, 7, 2, 6),   -- 6 'tablespoons'
-       (200, 8, 2, 2), -- 2 'grams'
-       (2, 9, 2, 7),   -- 7 'teaspoons'
-       (1, 10, 2, 7),  -- 7 'teaspoon'
-       (4, 1, 2, 1),   -- 1 'cups'
-       (250, 2, 2, 2), -- 2 'grams'
-       (3, 3, 2, 3);
--- 3 'units'
+VALUES (1, 22, 2, 3),
+       (2, 23, 2, 3),
+       (2, 24, 2, 3),
+       (360, 25, 2, 2),
+       (8, 26, 2, 3),
+       (20, 27, 2, 2),
+       (1, 28, 2, 9),
+       (2, 29, 2, 7),
+       (60, 30, 2, 10),
+       (5, 31, 2, 2);
+
+INSERT INTO recipe_ingredient (count, ingredient_id, recipe_id, measure_id)
+VALUES (220, 19, 3, 10),
+       (20, 32, 3, 2),
+       (0.5, 9, 3, 7),
+       (40, 33, 3, 2),
+       (375, 34, 3, 2),
+       (3, 35, 3, 6),
+       (1, 36, 3, 3);
+
+INSERT INTO recipe_ingredient (count, ingredient_id, recipe_id, measure_id)
+VALUES (25, 32, 4, 2),
+       (150, 30, 4, 10),
+       (0.5, 35, 4, 7),
+       (250, 34, 4, 2),
+       (1, 9, 4, 7),
+       (1, 7, 4, 6),
+       (1, 10, 4, 11),
+       (0.5, 37, 4, 7),
+       (2, 38, 4, 3),
+       (6, 6, 4, 11);
+
+INSERT INTO recipe_ingredient (count, ingredient_id, recipe_id, measure_id)
+VALUES (450, 39, 5, 2),
+       (1, 40, 5, 6),
+       (5, 41, 5, 6),
+       (2, 29, 5, 6),
+       (2, 42, 5, 6),
+       (2, 5, 5, 5),
+       (2, 43, 5, 7),
+       (100, 44, 5, 2),
+       (0.5, 45, 5, 3),
+       (1, 7, 5, 7),
+       (0.5, 46, 5, 1),
+       (1, 47, 5, 7),
+       (2, 31, 5, 6),
+
+
+INSERT INTO recipe_ingredient (count, ingredient_id, recipe_id, measure_id)
+VALUES (1, 48, 6, 1),
+       (250, 49, 6, 2),
+       (200, 50, 6, 2),
+       (2, 35, 6, 6);
+
+INSERT INTO recipe_ingredient (count, ingredient_id, recipe_id, measure_id)
+VALUES (400, 34, 7, 2),
+       (4, 51, 7, 3),
+       (1, 9, 7, 11),
+       (400, 15, 7, 2),
+       (50, 20, 7, 2),
+       (200, 52, 7, 10),
+       (50, 16, 7, 10),
+       (1, 4, 7, 3),
+       (1, 18, 7, 6),
+       (1, 37, 7, 7),
+       (1, 53, 7, 7),
+       (2, 5, 7, 5),
+
+INSERT INTO recipe_ingredient (count, ingredient_id, recipe_id, measure_id)
+VALUES (500, 54, 8, 2),
+       (30, 55, 8, 10),
+       (4, 41, 8, 6),
+       (2, 56, 8, 6),
+       (4, 40, 8, 6),
+       (1, 57, 8, 13),
+       ( 1, 10, 8, 11),
+       (10, 43, 8, 2),
+       (1, 58, 8, 2);
+
+INSERT INTO recipe_ingredient (count, ingredient_id, recipe_id, measure_id)
+VALUES (225, 34, 9, 2),
+       (5, 35, 9, 6),
+       (1, 59, 9, 7),
+       (150, 33, 9, 2),
+       (1, 51, 9, 3),
+       (400, 60, 9, 2);
+
+INSERT INTO recipe_ingredient (count, ingredient_id, recipe_id, measure_id)
+VALUES (1500, 61, 12, 2),
+       (320, 34, 12, 2),
+       (200, 33, 12, 2),
+       (1, 51, 12, 3),
+       (0.25, 35, 12, 1),
+       (2, 59, 12, 7);
+
 
 -- Insert records into the recipe_step table
 INSERT INTO recipe_step (step_order, title, description, recipe_id)
